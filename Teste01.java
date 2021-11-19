738624f49ac2c6743b35ee436a3f3b93882fd336
Arilms <94670296+Arilms@users.noreply.github.com>
2021-11-19 04:18:23 -0300

    Create Teste01.java

---
+++ Teste01.java
@@ -0,0 +1,14 @@
+public class Teste01
+{
+    private int x;
+
+    public Teste01()
+    {
+        x = 0;
+    }
+  
+    public int sampleMethod(int y)      
+    {
+        return x + y;
+    }
+}
